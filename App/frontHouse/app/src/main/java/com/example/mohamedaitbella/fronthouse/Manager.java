04778f0fd679f890ae569c127660c48756e05123
Anirudh Singh <anirudhsurya94@gmail.com>
2018-07-31 19:44:02 -0400

    layout added for Bernardin

---
+++ App/frontHouse/app/src/main/java/com/example/mohamedaitbella/fronthouse/Manager.java
@@ -0,0 +1,13 @@
+package com.example.mohamedaitbella.fronthouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Manager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+    }
+}
